191fce4ec4810b5b075d33756ac8ff54e7b79da2
Vitor Pereira <vitorpereiradeoli@gmail.com>
2022-10-09 17:45:14 -0300

    :wrench: dockerize app

---
+++ database/migrations/2022_10_09_192823_create_teacher_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacher', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->decimal('rating')->default(0);
+            $table->integer('evaluations')->default(0);
+            $table->foreignId('department_id')->constrained('department');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacher');
+    }
+};
