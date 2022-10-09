049e2f32e093703f0f1ad535af1f01203991b7b4
Vitor Pereira <vitorpereiradeoli@gmail.com>
2022-10-09 18:14:57 -0300

    :ambulance: fix teacher migration file issue

--- database/migrations/2022_10_09_192823_create_teacher_table.php
+++ database/migrations/2022_10_09_192823_create_teacher_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('teacher', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->nullable();
             $table->decimal('rating')->default(0);
             $table->integer('evaluations')->default(0);
             $table->foreignId('department_id')->constrained('department');
